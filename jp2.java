0a3562c7c9e03162c3988ae5c941b2e394df17ce
aaryanmehta2307 <118744251+aaryanmehta2307@users.noreply.github.com>
2024-09-13 10:35:27 +0530

    Update jp2.java

--- jp2.java
+++ jp2.java
@@ -1,22 +1,12 @@
-import java.util.Scanner;
-
-public class AddTwoNumbers {
+public class SquarePattern {
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
-        Scanner scanner = new Scanner(System.in);
+        int n = 5; // Size of the pattern
 
-        // Take input for the first number
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        // Take input for the second number
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        // Add the two numbers
-        int sum = num1 + num2;
-
-        // Print the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
